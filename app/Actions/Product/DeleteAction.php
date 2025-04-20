abb520b26bfe1f8a07351373707a63505dd4c06d
raheescv <raheescv1992@gmail.com>
2025-04-20 17:20:22 +0530

    Update DeleteAction.php

--- app/Actions/Product/DeleteAction.php
+++ app/Actions/Product/DeleteAction.php
@@ -3,6 +3,10 @@
 namespace App\Actions\Product;
 
 use App\Models\Product;
+use App\Models\PurchaseItem;
+use App\Models\SaleItem;
+use App\Models\SaleReturnItem;
+use Exception;
 
 class DeleteAction
 {
@@ -11,11 +15,27 @@ class DeleteAction
         try {
             $model = Product::find($id);
             if (! $model) {
-                throw new \Exception("Resource not found with the specified ID: $id.", 1);
+                throw new Exception("Resource not found with the specified ID: $id.", 1);
             }
             if (! $model->delete()) {
-                throw new \Exception('Oops! Something went wrong while deleting the Product. Please try again.', 1);
+                throw new Exception('Oops! Something went wrong while deleting the Product. Please try again.', 1);
             }
+
+            $saleItemCount = SaleItem::where('product_id', $id)->count();
+            if ($saleItemCount) {
+                throw new Exception('product already used in sales.', 1);
+            }
+
+            $saleReturnItemCount = SaleReturnItem::where('product_id', $id)->count();
+            if ($saleReturnItemCount) {
+                throw new Exception('product already used in sales return.', 1);
+            }
+
+            $purchaseItemCount = PurchaseItem::where('product_id', $id)->count();
+            if ($purchaseItemCount) {
+                throw new Exception('product already used in Purchases.', 1);
+            }
+
             $model->inventories()->delete();
 
             $return['success'] = true;
