7cee6666ace42fb32c548f61719ea098392de08a
raheescv <raheescv1992@gmail.com>
2025-05-01 22:22:55 +0530

    dattable added

--- resources/views/livewire/report/sale/overview-report.blade.php
+++ resources/views/livewire/report/sale/overview-report.blade.php
@@ -20,9 +20,9 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-md-2">
+        <div class="col-md-12">
             <div class="card mb-3">
-                <div class="card-header bg-secondary text-white">
+                <div class="card-header">
                     <h6 class="card-title mb-0">Overview</h6>
                 </div>
                 <div class="card-body p-2">
@@ -41,14 +41,16 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-5">
+    </div>
+    <div class="row">
+        <div class="col-md-6">
             <div class="card mb-3">
-                <div class="card-header bg-secondary text-white">
+                <div class="card-header">
                     <h6 class="card-title mb-0">Employee Sales</h6>
                 </div>
                 <div class="card-body p-2">
                     <div class="table-responsive">
-                        <table class="table table-sm align-middle">
+                        <table class="table table-sm align-middle" id="employeeDataTable">
                             <thead class="table-light">
                                 <tr>
                                     <th>#</th>
@@ -79,14 +81,14 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-5">
+        <div class="col-md-6">
             <div class="card mb-3">
-                <div class="card-header bg-secondary text-white">
+                <div class="card-header">
                     <h6 class="card-title mb-0">Product/Service Sales</h6>
                 </div>
                 <div class="card-body p-2">
                     <div class="table-responsive">
-                        <table class="table table-sm align-middle">
+                        <table class="table table-sm align-middle" id="productDataTable">
                             <thead class="table-light">
                                 <tr>
                                     <th>#</th>
@@ -118,6 +120,9 @@
             </div>
         </div>
     </div>
+    @push('styles')
+        <link rel="stylesheet" href="{{ asset('assets/vendors/dataTable/datatables.min.css') }}">
+    @endpush
     @push('scripts')
         <script>
             $(document).ready(function() {
@@ -125,7 +130,105 @@
                     const value = $(this).val() || null;
                     @this.set('branch_id', value);
                 });
+
+                employeeDataTable = new DataTable('#employeeDataTable', {
+                    paging: false, // Disable paging
+                    scrollY: '400px',
+                    scrollCollapse: true,
+                    order: [
+                        [3, 'desc']
+                    ],
+                    dom: '<"d-flex justify-content-between align-items-center mb-3"<"d-flex"B><"ml-auto"f>>t', // Add margin-bottom to the container
+                    buttons: [{
+                            extend: 'excelHtml5',
+                            text: 'Excel',
+                            title: 'Employee Sales',
+                            className: 'btn btn-primary btn-sm'
+                        },
+                        {
+                            extend: 'pdfHtml5',
+                            text: 'PDF',
+                            title: 'Employee Sales',
+                            className: 'btn btn-secondary btn-sm',
+                            customize: function(doc) {
+                                // Set table width to 100%
+                                doc.content[1].table.widths = ['10%', '50%', '20%', '20%']; // Product column set to 50%
+                                // Align headers to match table alignment
+                                doc.styles.tableHeader = {
+                                    alignment: 'left', // Center align headers by default
+                                    bold: true,
+                                    fontSize: 12,
+                                    color: 'black'
+                                };
+
+                                // Ensure "Quantity" and "Total" columns are right-aligned in the body
+                                doc.content[1].table.body.forEach(function(row, rowIndex) {
+                                    if (rowIndex === 0) {
+                                        row[0].alignment = 'right';
+                                        row[2].alignment = 'right';
+                                        row[3].alignment = 'right';
+                                    } else {
+                                        row[0].alignment = 'right';
+                                        row[2].alignment = 'right';
+                                        row[3].alignment = 'right';
+                                    }
+                                });
+                            }
+                        }
+                    ]
+                });
+
+                productDataTable = new DataTable('#productDataTable', {
+                    scrollY: '400px',
+                    scrollCollapse: true,
+                    order: [
+                        [3, 'desc']
+                    ],
+                    dom: '<"d-flex justify-content-between align-items-center mb-3"<"d-flex"B><"ml-auto"f>>t', // Add margin-bottom to the container
+                    buttons: [{
+                            extend: 'excelHtml5',
+                            text: 'Excel',
+                            title: 'Product Sales',
+                            className: 'btn btn-primary btn-sm'
+                        },
+                        {
+                            extend: 'pdfHtml5',
+                            text: 'PDF',
+                            title: 'Product Sales',
+                            className: 'btn btn-secondary btn-sm',
+                            customize: function(doc) {
+                                // Set specific column widths
+                                doc.content[1].table.widths = ['10%', '50%', '20%', '20%']; // Product column set to 50%
+
+                                // Align headers to match table alignment
+                                doc.styles.tableHeader = {
+                                    alignment: 'center', // Center align headers by default
+                                    bold: true,
+                                    fontSize: 12,
+                                    color: 'black'
+                                };
+
+                                // Ensure "Quantity" and "Total" columns are right-aligned in the body
+                                doc.content[1].table.body.forEach(function(row, rowIndex) {
+                                    if (rowIndex === 0) {
+                                        row[0].alignment = 'right'; // # header
+                                        row[2].alignment = 'right'; // Quantity header
+                                        row[3].alignment = 'right'; // Total header
+                                    } else {
+                                        row[0].alignment = 'right'; // # body
+                                        row[2].alignment = 'right'; // Quantity body
+                                        row[3].alignment = 'right'; // Total body
+                                    }
+                                });
+                            }
+                        }
+                    ]
+                });
             });
         </script>
+        <script src="{{ asset('assets/vendors/dataTable/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('assets/vendors/dataTable/pdfmake.min.js') }}"></script>
+        <script src="{{ asset('assets/vendors/dataTable/vfs_fonts.js') }}"></script>
+        <script src="{{ asset('assets/vendors/dataTable/datatables.min.js') }}"></script>
     @endpush
 </div>
