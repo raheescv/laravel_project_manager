27553fe03ff8d5e24a7246a54587a5e6be8009d5
raheescv <raheescv1992@gmail.com>
2025-05-01 15:26:05 +0530

    backup

---
+++ routes/fyers_console.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\Storage;
+
+$headers = [
+    'token', 'symbol_name', 'lot_size', 'strike_price', 'premium', 'empty_field',
+    'market_hours', 'expiry_date', 'timestamp', 'exchange_symbol', 'bid_qty', 'ask_qty',
+    'trade_token', 'underlying', 'open_interest', 'strike_price_2', 'option_type',
+    'fyToken', 'extra_field_1', 'extra_field_2', 'extra_field_3',
+];
+$contents = Storage::get('symbols/NSE_FO.csv');
+if ($contents) {
+    $rows = explode("\n", trim($contents));
+    $csvArray = [];
+    foreach ($rows as $row) {
+        $values = str_getcsv($row);
+        $csvArray[] = array_combine($headers, $values);
+    }
+    $list = collect($csvArray)
+        ->filter(function ($item) {
+            return $item['underlying'] === 'NIFTY' && str_contains($item['symbol_name'], 'Mar 06');
+        })
+        ->unique('exchange_symbol');
+
+    // info('list count : '.count($list));
+
+    foreach ($list as $value) {
+        $no_data_symbols = cache('no_data_symbols', []);
+        if (! in_array($value['exchange_symbol'], $no_data_symbols)) {
+            Schedule::command('trade:intra-day '.$value['exchange_symbol'].' 75')->everyFiveMinutes();
+        }
+    }
+
+    // $list = collect($csvArray)
+    //     ->filter(function ($item) {
+    //         return $item['underlying'] === 'BANKNIFTY' && str_contains($item['symbol_name'], 'Feb');
+    //     })
+    //     ->unique('exchange_symbol');
+    // foreach ($list as $key => $value) {
+    //     Schedule::command('trade:intra-day '.$value['exchange_symbol'].' 30')->everyFiveMinutes();
+    // }
+
+    Schedule::command('app:intra-day-selling')->everyMinute();
+
+}
