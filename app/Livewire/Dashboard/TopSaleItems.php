6e665d100344388fb03c356b0d80131251a89b44
raheescv <raheescv1992@gmail.com>
2025-05-02 12:18:13 +0530

    Update TopSaleItems.php

--- app/Livewire/Dashboard/TopSaleItems.php
+++ app/Livewire/Dashboard/TopSaleItems.php
@@ -10,7 +10,12 @@ class TopSaleItems extends Component
 {
     public function render()
     {
-        $topItems = SaleItem::with('product')
+        $topItems = SaleItem::with('product:id,name')
+            ->whereHas('sale', function ($query) {
+                return $query
+                    ->where('branch_id', session('branch_id'))
+                    ->where('status', 'completed');
+            })
             ->whereDate('created_at', Carbon::today())
             ->selectRaw('product_id, SUM(quantity) as total_quantity, SUM(total) as total_amount')
             ->groupBy('product_id')
