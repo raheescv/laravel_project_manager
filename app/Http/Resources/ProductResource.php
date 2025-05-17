d98ff9354459514a0b9fbc4eee8c0113133ea72f
raheescv <raheescv1992@gmail.com>
2025-05-17 18:12:06 +0530

    Update ProductResource.php

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -11,20 +11,19 @@ class ProductResource extends JsonResource
     {
         return [
             'id' => (string) $this->id,
-            'type' => $this->type,
-            'code' => $this->code,
+            // 'type' => $this->type,
+            // 'code' => $this->code,
             'name' => $this->name,
-            'name_arabic' => $this->name_arabic,
+            // 'name_arabic' => $this->name_arabic,
             'thumbnail' => $this->thumbnail,
             'unit' => $this->unit?->name,
-            'main_category' => $this->mainCategory?->name,
-            'hsn_code' => $this->hsn_code,
+            'category' => $this->mainCategory?->name,
+            // 'hsn_code' => $this->hsn_code,
             'tax' => $this->tax,
             'description' => $this->description,
-            'is_selling' => (bool) $this->is_selling,
             'is_favorite' => (bool) $this->is_favorite,
             'mrp' => (float) $this->mrp,
-            'barcode' => $this->barcode,
+            // 'barcode' => $this->barcode,
         ];
     }
 }
