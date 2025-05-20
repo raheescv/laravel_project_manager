1915dd89998241f3d86ca2afeaf4e2fe4ae5fa71
raheescv <raheescv1992@gmail.com>
2025-05-20 13:59:56 +0530

    Update TrackVisitor.php

--- app/Http/Middleware/TrackVisitor.php
+++ app/Http/Middleware/TrackVisitor.php
@@ -44,8 +44,8 @@ class TrackVisitor
             && ! $request->is('api/*')
             && ! $request->is('_debugbar/*')
             && ! $request->is('livewire/*')
-            && ! str_contains($path, '/')
-            && ! str_contains($path, 'list')
+            && ! str_contains($path, '/') // skip dashboard
+            && ! str_contains($path, 'list') // list get list for select
             && ! str_contains($path, '.')  // Skip files with extensions (assets)
             && ! app()->runningUnitTests();
     }
