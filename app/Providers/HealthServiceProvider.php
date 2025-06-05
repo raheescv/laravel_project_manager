1a33367e297b3411159fe002f6fa59b9df4fd609
raheescv <raheescv1992@gmail.com>
2025-06-05 20:32:59 +0530

    Update HealthServiceProvider.php

--- app/Providers/HealthServiceProvider.php
+++ app/Providers/HealthServiceProvider.php
@@ -3,18 +3,16 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Spatie\Health\Facades\Health;
 use Spatie\Health\Checks\Checks\CacheCheck;
 use Spatie\Health\Checks\Checks\DatabaseCheck;
 use Spatie\Health\Checks\Checks\DatabaseConnectionCountCheck;
 use Spatie\Health\Checks\Checks\DebugModeCheck;
 use Spatie\Health\Checks\Checks\EnvironmentCheck;
-use Spatie\Health\Checks\Checks\HorizonCheck;
 use Spatie\Health\Checks\Checks\OptimizedAppCheck;
 use Spatie\Health\Checks\Checks\QueueCheck;
 use Spatie\Health\Checks\Checks\ScheduleCheck;
 use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
-use Spatie\Health\Checks\Checks\BackupsCheck;
+use Spatie\Health\Facades\Health;
 
 class HealthServiceProvider extends ServiceProvider
 {
@@ -26,17 +24,17 @@ class HealthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $environment = app()->environment();
-        
+
         $checks = [
             // Core Laravel Checks
             DatabaseCheck::new(),
             CacheCheck::new(),
-            
+
             // Database Performance
             DatabaseConnectionCountCheck::new()
                 ->warnWhenMoreConnectionsThan(50)
                 ->failWhenMoreConnectionsThan(100),
-            
+
             // System Resources
             UsedDiskSpaceCheck::new()
                 ->warnWhenUsedSpaceIsAbovePercentage(70)
@@ -50,11 +48,6 @@ class HealthServiceProvider extends ServiceProvider
             $checks[] = OptimizedAppCheck::new();
             $checks[] = QueueCheck::new();
             $checks[] = ScheduleCheck::new()->heartbeatMaxAgeInMinutes(2);
-            
-            // Only add Horizon check if Redis is configured
-            if (config('database.redis.default')) {
-                $checks[] = HorizonCheck::new();
-            }
         } else {
             // Development environment checks
             $checks[] = EnvironmentCheck::new()->expectEnvironment($environment);
