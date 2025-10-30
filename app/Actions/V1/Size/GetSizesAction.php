227d217633a5fa55ad0f98f0518c53e2a0fabf69
raheescv <raheescv1992@gmail.com>
2025-10-30 12:39:54 +0530

    Update GetSizesAction.php

--- app/Actions/V1/Size/GetSizesAction.php
+++ app/Actions/V1/Size/GetSizesAction.php
@@ -14,7 +14,7 @@ class GetSizesAction
     {
         $filters = $request->validatedWithDefaults();
 
-        $query = Product::selectRaw('size, COUNT(*) as product_count')
+        $query = Product::selectRaw('size')
             ->when($filters['code'] ?? null, function ($q, $value) {
                 return $q->where('code', 'like', "%{$value}%");
             })
@@ -23,13 +23,50 @@ class GetSizesAction
             ->groupBy('size')
             ->orderBy('size');
 
-        $sizes = $query->get();
+        $sizes = $query->get()->pluck('size')->filter()->values();
 
-        return $sizes->map(function ($size) {
-            return [
-                'size' => $size->size,
-                'product_count' => $size->product_count,
-            ];
-        })->toArray();
+        $kidsKeywords = ['KID', 'KIDS', 'BOY', 'BOYS', 'GIRL', 'GIRLS', 'BABY', 'INFANT', 'TODDLER', 'YR', 'YRS', 'YEAR', 'YEARS'];
+
+        $isKidsSize = function (string $size) use ($kidsKeywords): bool {
+            $upper = strtoupper(trim($size));
+
+            foreach ($kidsKeywords as $kw) {
+                if (str_contains($upper, $kw)) {
+                    return true;
+                }
+            }
+
+            // Numeric-only sizes up to 34 considered kids by default (tweak if needed)
+            if (preg_match('/^\d{1,2}$/', $upper)) {
+                return (int) $upper <= 34;
+            }
+
+            // Common kids patterns like 0-3M, 6-12M, 2-3Y, etc.
+            if (preg_match('/^(\d+\s*-\s*\d+\s*)(M|MOS|MONTH|MONTHS|Y|YR|YRS|YEAR|YEARS)$/i', $upper)) {
+                return true;
+            }
+            if (preg_match('/^(\d+)(M|MOS|MONTH|MONTHS|Y|YR|YRS|YEAR|YEARS)$/i', $upper)) {
+                return true;
+            }
+
+            return false;
+        };
+
+        $kids = [];
+        $others = [];
+
+        foreach ($sizes as $size) {
+            $item = ['size' => $size];
+            if ($isKidsSize((string) $size)) {
+                $kids[] = $item;
+            } else {
+                $others[] = $item;
+            }
+        }
+
+        return [
+            'kids_sizes' => $kids,
+            'other_sizes' => $others,
+        ];
     }
 }
