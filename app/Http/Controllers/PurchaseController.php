3a7424b660570bbbee1b3490e65fef36abc2181a
raheescv <raheescv1992@gmail.com>
2025-11-22 11:30:26 +0530

    Update PurchaseController.php

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -33,44 +33,6 @@ class PurchaseController extends Controller
         return view('purchase.page', compact('id'));
     }
 
-    // public function barcodePrint($id)
-    // {
-    //     $purchaseItems = PurchaseItem::with('product')->where('purchase_id', $id)->get();
-
-    //     if (empty($purchaseItems)) {
-    //         return redirect()->route('purchase::index')->with('error', 'No items to print. Please add products to cart first.');
-    //     }
-    //     // Load barcode settings
-    //     $settings = Configuration::where('key', 'barcode_configurations')->value('value');
-    //     $settings = json_decode($settings, true) ?? [];
-    //     $company_name = Configuration::where('key', 'company_name')->value('value') ?? config('app.name');
-
-    //     // Generate HTML using Blade view
-    //     $html = view('purchase.barcode-print', compact('purchaseItems', 'settings', 'company_name'))->render();
-
-    //     // Use the same PDF settings as the existing barcode print method
-    //     $pdf = Browsershot::html($html)
-    //         ->paperSize($settings['width'], $settings['height'])
-    //         ->noSandbox()
-    //         ->setNodeBinary('/usr/local/bin/node')
-    //         ->setNpmBinary('/usr/local/bin/npm')
-    //         ->ignoreHttpsErrors()
-    //         ->disableJavascript()
-    //         ->blockDomains(['*']) // Block external resource loading
-    //         ->setOption('args', ['--disable-web-security', '--no-sandbox', '--disable-gpu'])
-    //         ->margins(0, 0, 0, 0)
-    //         ->deviceScaleFactor(1)
-    //     // ->windowSize($settings['height'], $settings['width'])
-    //         ->pdf([
-    //             'printBackground' => false,
-    //             'preferCSSPageSize' => true,
-    //             'scale' => 1,
-    //         ]);
-
-    //     return response($pdf)
-    //         ->header('Content-Type', 'application/pdf')
-    //         ->header('Content-Disposition', 'inline; filename="cart-barcode-'.time().'.pdf"');
-    // }
     public function barcodePrint($id)
     {
         $purchaseItems = PurchaseItem::with('product')->where('purchase_id', $id)->get();
