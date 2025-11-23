ab3b2b8bf5bd2051493f67af9d4f4617c324d9da
raheescv <raheescv1992@gmail.com>
2025-11-23 14:32:42 +0530

    Update api_log.php

--- routes/api_log.php
+++ routes/api_log.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function (): void {
     Route::name('api_log::')->prefix('api_log')->controller(ApiLogController::class)->group(function (): void {
-        Route::get('', 'index')->name('index')->name('index')->can('api_log.view');
-        Route::get('moq-settings', 'moqSettings')->name('moq-settings')->name('moq-settings')->can('api_log.moq settings');
+        Route::get('', 'index')->name('index')->can('api_log.view');
+        Route::get('moq-settings', 'moqSettings')->name('moq-settings')->can('api_log.moq settings');
     });
 });
