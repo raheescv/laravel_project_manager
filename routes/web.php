14a089fba795d92b665d8be95eb32b83474f61bf
raheescv <raheescv1992@gmail.com>
2025-11-23 13:18:16 +0530

    public_home

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,12 @@ use Illuminate\Support\Facades\Route;
 
 require __DIR__.'/auth.php';
 
-Route::get('/home', [PublicScanController::class, 'home'])->name('home');
+Route::get('/home', [PublicScanController::class, 'home'])->name('public_home');
 Route::get('/scan', [PublicScanController::class, 'index'])->name('scan.index');
 Route::post('/scan/search', [PublicScanController::class, 'search'])->name('scan.search');
 Route::middleware('auth')->group(function (): void {
     Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
-    Route::get('/', [HomeController::class, 'index'])->name('home');
+    // Route::get('/', [HomeController::class, 'index'])->name('home');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -56,7 +56,7 @@ Route::middleware('auth')->group(function (): void {
         Route::get('employee-calendar', 'calendar')->name('index')->can('appointment.view');
     });
 
-    Route::get('generate-image', [ImageGenComfyController::class, 'generate']);
+    Route::get('generate-image', [ImageGenComfyController::class, 'generate'])->name('generate-image');
     Route::get('family-tree', [FamilyTreeController::class, 'index'])->name('family-tree'); // ->can('family-tree.view');
     Route::get('visitor-analytics', [VisitorAnalyticsController::class, 'index'])->name('visitor-analytics')->can('visitor analytics.view');
 
