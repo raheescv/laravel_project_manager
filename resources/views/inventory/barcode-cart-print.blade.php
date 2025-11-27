f9c6b6ada973156fe28968f799599cbf7720d719
raheescv <Raheescv1992@gmail.com>
2025-11-27 03:46:20 -0800

    barcode revert

--- resources/views/inventory/barcode-cart-print.blade.php
+++ resources/views/inventory/barcode-cart-print.blade.php
@@ -59,7 +59,7 @@
             transition: all 0.3s ease;
         }
 
-        /* .product-name {
+        .product-name {
             font-size: {{ $settings['product_name']['font_size'] }}px;
             text-align: {{ $settings['product_name']['align'] }};
             line-height: 1.1;
@@ -68,18 +68,7 @@
             overflow: hidden;
             text-overflow: ellipsis;
             margin-bottom: 2px;
-        } */
-            .product-name {
-    font-size: {{ $settings['product_name']['font_size'] }}px;
-    text-align: {{ $settings['product_name']['align'] }};
-    line-height: 1.2;
-    font-weight: 600;
-    white-space: normal; /* allow wrap */
-    overflow: visible;   /* no cut */
-    text-overflow: unset;
-    margin-bottom: 6px;
-}
-
+        }
 
         .product-name-arabic {
             font-size: {{ $settings['product_name_arabic']['font_size'] }}px;
@@ -190,8 +179,7 @@
                     <div class="barcode-container">
                         @if ($settings['product_name']['visible'] ?? true)
                             <div id="product-name" class="barcode-element product-name" draggable="true" style="{{ getElementStyle('product_name', $settings) }}">
-                                <!-- <b>{{ substr($inventory->product->name, 0, (int) $settings['product_name']['char_limit']) }}</b> -->
-                                 <b>{{ rtrim(substr($inventory->product->name, 0, (int) $settings['product_name']['char_limit'])) . ' ' }}</b>
+                                <b>{{ substr($inventory->product->name, 0, (int) $settings['product_name']['char_limit']) }}</b>
                                 <div class="element-handle top-left"></div>
                                 <div class="element-handle top-right"></div>
                             </div>
