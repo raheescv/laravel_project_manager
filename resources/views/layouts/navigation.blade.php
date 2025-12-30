d9195fd9b6a88891f1488b2615b60bebfa6216bd
raheescv <raheescv1992@gmail.com>
2025-12-30 15:53:36 +0300

    package added

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -71,6 +71,11 @@
         }
 
         /* Teal for FlatTrade */
+        .nav-link .fa-gift {
+            color: #FF6B6B;
+        }
+
+        /* Coral for Package */
 
         /* Hover effects for icons */
         .nav-link:hover .fa {
@@ -362,6 +367,28 @@
                                 @can('purchase return.payments')
                                     <li class="nav-item">
                                         <a href="{{ route('purchase_return::payments') }}" class="nav-link {{ request()->is(['purchase_return/payments']) ? 'active' : '' }}">Payments</a>
+                                    </li>
+                                @endcan
+                            </ul>
+                        </li>
+                    @endif
+                    @if (auth()->user()->can('package.view') || auth()->user()->can('package.create'))
+                        <li class="nav-item has-sub">
+                            <a href="#"
+                                class="mininav-toggle nav-link {{ request()->is(['package', 'package/create', 'package/edit/*']) ? 'active' : '' }}"><i
+                                    class="fa fa-gift fs-5 me-2"></i>
+                                <span class="nav-label mininav-content ms-1 collapse show" style="">Package</span>
+                            </a>
+                            <ul class="mininav-content nav collapse">
+                                <li data-popper-arrow class="arrow"></li>
+                                @can('package.create')
+                                    <li class="nav-item">
+                                        <a href="{{ route('package::create') }}" class="nav-link {{ request()->is(['package/create']) ? 'active' : '' }}">Create</a>
+                                    </li>
+                                @endcan
+                                @can('package.view')
+                                    <li class="nav-item">
+                                        <a href="{{ route('package::index') }}" class="nav-link {{ request()->is(['package', 'package/edit/*']) ? 'active' : '' }}">List</a>
                                     </li>
                                 @endcan
                             </ul>
