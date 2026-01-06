60c9a4d3fc80f1671e073cda7fffb20478c1fa24
raheescv <Raheescv1992@gmail.com>
2026-01-06 17:50:16 +0530

    bugs

--- database/migrations/2025_12_09_135956_add_name_to_measurement_templates_table.php
+++ database/migrations/2025_12_09_135956_add_name_to_measurement_templates_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::table('measurement_templates', function (Blueprint $table) {
              $table->string('name')->after('category_id')->nullable();
+             
         });
+
     }
 
     /**
