ed0d21984ae8e6268152abe3a7bf4b8d540448dc
raheescv <raheescv1992@gmail.com>
2026-01-12 10:39:57 +0300

    Update 2026_01_20_000001_add_data_to_tenants_table.php

--- database/migrations/2026_01_20_000001_add_data_to_tenants_table.php
+++ database/migrations/2026_01_20_000001_add_data_to_tenants_table.php
@@ -1,26 +1,20 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 
 return new class() extends Migration
 {
     public function up(): void
     {
-        DB::table('tenants')->insertOrIgnore([
-            'name' => 'Admin',
-            'code' => 'admin',
-            'subdomain' => 'project_manager',
-            'domain' => 'project_manager.test',
-            'is_active' => 1,
-            'description' => 'Admin tenant',
-            'created_at' => now(),
-            'updated_at' => now(),
+        Artisan::call('db:seed', [
+            '--class' => 'Database\\Seeders\\TenantSeeder',
+            '--force' => true,
         ]);
     }
 
     public function down(): void
     {
-        DB::table('configurations')->where('key', 'theme_settings')->delete();
     }
 };
