3b425dd6f09b415c05ba73b39d42b76ba40a727b
raheescv <raheescv1992@gmail.com>
2026-01-12 21:17:08 +0300

    unit page updated

--- resources/views/components/sortable-header.blade.php
+++ resources/views/components/sortable-header.blade.php
@@ -1,6 +1,9 @@
-<a href="#" wire:click.prevent="sortBy('{{ $field }}')">
+<a href="#" class="text-decoration-none text-dark d-flex align-items-center gap-1" wire:click.prevent="sortBy('{{ $field }}')">
     {{ ucWords($label) }}
     @if ($sortField === $field)
-        {!! sortDirection($direction) !!}
+        <small>{!! sortDirection($direction ?? $sortDirection) !!}</small>
+    @else
+        <small class="text-muted opacity-50"><i class="demo-psi-arrow-up-down fs-6"></i></small>
     @endif
 </a>
+
