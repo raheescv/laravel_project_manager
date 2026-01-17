0e0c54f235d780872fe8c1f509877ee5b08b0878
raheescv <raheescv1992@gmail.com>
2026-01-17 13:54:49 +0300

    pint

--- resources/views/livewire/report/day-book-report.blade.php
+++ resources/views/livewire/report/day-book-report.blade.php
@@ -63,7 +63,8 @@
                             <td>{{ $item->id }}</td>
                             <td>{{ systemDate($item->date) }}</td>
                             <td>
-                                <a href="{{ route('account::view', $item->account_id) }}?from_date={{ $from_date }}&to_date={{ $to_date }}" class="text-decoration-none" style="padding-left: 3rem !important; display: block;">{{ $item->account_name }}</a>
+                                <a href="{{ route('account::view', $item->account_id) }}?from_date={{ $from_date }}&to_date={{ $to_date }}" class="text-decoration-none"
+                                    style="padding-left: 3rem !important; display: block;">{{ $item->account_name }}</a>
                             </td>
                             <td>
                                 @switch($item->model)
