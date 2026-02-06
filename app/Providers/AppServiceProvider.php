a8be1134fafbee81af5ffa2e0b91d72f733da61a
raheescv <raheescv1992@gmail.com>
2026-02-06 21:05:48 +0530

    horizon

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -44,7 +44,7 @@ class AppServiceProvider extends ServiceProvider
                 );
             });
         // Force HTTPS for assets when the app is served over HTTPS
-        if (request()->isSecure() || config('constants.force_https', false) || config('app.url', '')->startsWith('https://')) {
+        if (request()->isSecure() || config('constants.force_https', false) || str_starts_with(config('app.url', ''), 'https://')) {
             URL::forceScheme('https');
         }
 
