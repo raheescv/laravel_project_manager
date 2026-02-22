4cc0b670d6d19ae4bf1adf4b2b06aacb5cfda127
raheescv <raheescv1992@gmail.com>
2026-02-22 11:19:14 +0530

    day-session moved to new navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -366,9 +366,6 @@
                                     'sale/view/*',
                                     'report/sale_item',
                                     'sale/receipts',
-                                    'sale/day-management',
-                                    'sale/day-sessions-report',
-                                    'sale/day-session/*',
                                 ];
                             @endphp
                             <a href="#" class="mininav-toggle nav-link {{ request()->is($list) ? 'active' : '' }}"><i class="fa fa-shopping-cart fs-5 me-2"></i>
@@ -396,12 +393,30 @@
                                         <a href="{{ route('sale::receipts') }}" class="nav-link {{ request()->is(['sale/receipts']) ? 'active' : '' }}">Receipts</a>
                                     </li>
                                 @endcan
-                                @can('sale.view')
+                            </ul>
+                        </li>
+                    @endif
+
+                    @if (auth()->user()->can('day session.list') || auth()->user()->can('day session.print'))
+                        <li class="nav-item has-sub">
+                            @php
+                                $list = [
+                                    'sale/day-management',
+                                    'sale/day-sessions-report',
+                                    'sale/day-session/*',
+                                ];
+                            @endphp
+                            <a href="#" class="mininav-toggle nav-link {{ request()->is($list) ? 'active' : '' }}"><i class="fa fa-shopping-cart fs-5 me-2"></i>
+                                <span class="nav-label mininav-content ms-1 collapse show" style="">Day Session</span>
+                            </a>
+                            <ul class="mininav-content nav collapse">
+                                <li data-popper-arrow class="arrow"></li>
+                                @can('day session.view')
                                     <li class="nav-item">
                                         <a href="{{ route('sale::day-management') }}" class="nav-link {{ request()->is(['sale/day-management']) ? 'active' : '' }}">Day Management</a>
                                     </li>
                                 @endcan
-                                @can('sale.view')
+                                @can('day session.list')
                                     <li class="nav-item">
                                         <a href="{{ route('sale::day-sessions-report') }}" class="nav-link {{ request()->is(['sale/day-sessions-report', 'sale/day-session/*']) ? 'active' : '' }}">Day
                                             Sessions Report</a>
--- routes/print.php
+++ routes/print.php
@@ -7,7 +7,7 @@ Route::middleware('auth')->group(function (): void {
     Route::name('print::')->prefix('print')->controller(PrintController::class)->group(function (): void {
         Route::name('sale::')->prefix('sale')->group(function (): void {
             Route::get('invoice/{id}', 'saleInvoice')->name('invoice');
-            Route::get('day-session-report/{id}', 'daySessionReport')->name('day-session-report');
+            Route::get('day-session-report/{id}', 'daySessionReport')->name('day-session-report')->can('day session.print');
             Route::get('customer-receipt', 'customerReceipt')->name('customer-receipt');
         });
         Route::name('sale_return::')->prefix('sale-return')->group(function (): void {
