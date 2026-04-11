e5ad026a6728415855fdfbcb1a5bc9deae4516bb
raheescv <raheescv1992@gmail.com>
2026-04-11 16:35:14 +0530

    Update cheque-status-modal.blade.php

--- resources/views/components/rent-out/cheque-status-modal.blade.php
+++ resources/views/components/rent-out/cheque-status-modal.blade.php
@@ -1,5 +1,5 @@
 <div class="modal" id="ChequeStatusModal" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
         <div class="modal-content border-0 shadow">
             @livewire('rent-out.tabs.cheque-status-modal')
         </div>
