b7b2455c47ff3e765416d759e7a70a05d665b9c5
raheescv <raheescv1992@gmail.com>
2026-05-23 13:04:37 +0530

    Update AuthUserResource.php

--- app/Http/Resources/V1/Auth/AuthUserResource.php
+++ app/Http/Resources/V1/Auth/AuthUserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\V1\Auth;
 
+use App\Models\SaleDaySession;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,11 @@ class AuthUserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $default_branch_id = $this->default_branch_id ? (string) $this->default_branch_id : null;
+        
+        $openingSession = SaleDaySession::getOpenSessionForBranch($default_branch_id);
+        $date = $openingSession ? $openingSession->opened_at->format('Y-m-d') : now()->format('Y-m-d');;
+
         return [
             'id' => (string) $this->id,
             'type' => $this->type,
@@ -23,7 +29,8 @@ class AuthUserResource extends JsonResource
             'mobile' => $this->mobile,
             'is_admin' => (bool) $this->is_admin,
             'designation' => $this->designation?->name,
-            'branch_id' => $this->default_branch_id ? (string) $this->default_branch_id : null,
+            'branch_id' => $default_branch_id,
+            'sale_day_session_date' => $date,
         ];
     }
 }
